a65ad2c08a24b619aaf087dc015b376a99e8affb
vladyslavtrubnikov <vladyslav.trubnikov@macc.com.ua>
2025-03-02 16:11:32 +0200

    ДЗ 24.1. POM in Playwright

--- tests/HW-23-1/invalid_UserRegistration.spec.js
+++ tests/HW-23-1/invalid_UserRegistration.spec.js
@@ -1,73 +1,43 @@
+
 import { test, expect } from '@playwright/test';
+import { RegistrationPage } from '../POM-HW-23-1/RegistrationPage';
 
 const BASE_URL = 'https://guest:welcome2qauto@qauto.forstudy.space';
-const EMAIL_PREFIX = 'aqa'; // Префікс для тестових юзерів
+const EMAIL_PREFIX = 'aqa';
 const TEST_DOMAIN = 'test.com';
 
-test.describe('User Registration', () => {
-    
+test.describe('Invalid User Registration', () => {
+    let registrationPage;
+
     test.beforeEach(async ({ page }) => {
-        await page.goto(BASE_URL);
-        await page.locator('button:has-text("Sign up")').click(); // Відкриваємо модальне вікно реєстрації
+        registrationPage = new RegistrationPage(page);
+        await registrationPage.navigate(BASE_URL);
+        await registrationPage.openSignUpModal();
     });
 
-
-    
-    test('Перевірка появи валідації на порожні поля', async ({ page }) => {
-        const nameInput = page.locator('input[name="name"]');
-        await nameInput.focus();
-        const lastNameInput = page.locator('input[name="lastName"]');
-        await lastNameInput.focus();
-        const mailInput = page.locator('input[name="email"]');
-        await mailInput.focus();
-        const passInput = page.locator('input[name="password"]');
-        await passInput.focus();
-        const repeatPassInput = page.locator('input[name="repeatPassword"]');
-        await repeatPassInput.focus();
-        const nameReturnInput = page.locator('input[name="name"]');
-        await nameReturnInput.focus();
-
-        await expect(page.locator('text="Name required"')).toBeVisible();
-        await expect(page.locator('text="Last name required"')).toBeVisible();
-        await expect(page.locator('text="Email required"')).toBeVisible();
-        await expect(page.locator('text="Password required"')).toBeVisible();
-        await expect(page.locator('text="Re-enter password required"')).toBeVisible();
+    test('Перевірка появи валідації на порожні поля', async () => {
+        await registrationPage.focusNameInput();
+        await registrationPage.focusLastNameInput();
+        await registrationPage.focusEmailInput();
+        await registrationPage.focusPassInput();
+        await expect(registrationPage.page.locator('text="Name required"')).toBeVisible();
+        await expect(registrationPage.page.locator('text="Last name required"')).toBeVisible();
+        await expect(registrationPage.page.locator('text="Email required"')).toBeVisible();
     });
 
-    
-    test('Перевірка на невалідний email', async ({ page }) => {
-        await page.fill('input[name="name"]', 'John');
-        await page.fill('input[name="lastName"]', 'Doe');
-        await page.fill('input[name="email"]', 'invalid-email');
-        await page.fill('input[name="password"]', 'Test1234');
-        await page.fill('input[name="repeatPassword"]', 'Test1234');
-        await expect(page.locator('text="Email is incorrect"')).toBeVisible();
+    test('Перевірка на невалідний email', async () => {
+        await registrationPage.fillRegistrationForm('John', 'Doe', 'invalid-email', 'Test1234', 'Test1234');
+        await expect(registrationPage.page.locator('text="Email is incorrect"')).toBeVisible();
     });
 
-    
-    test('Перевірка на внесення занадто короткого Імені', async ({ page }) => {
-        await page.fill('input[name="name"]', 'J'); // Тільки 1 символ
-        await page.fill('input[name="lastName"]', 'Doe');
-        await page.fill('input[name="email"]', `${EMAIL_PREFIX}-${Date.now()}@${TEST_DOMAIN}`);
-        await page.fill('input[name="password"]', 'Test1234');
-        await page.fill('input[name="repeatPassword"]', 'Test1234');
-        // await page.click('.modal-footer > .btn-primary');
-
-        await expect(page.locator('text="Name has to be from 2 to 20 characters long"')).toBeVisible();
+    test('Перевірка на внесення занадто короткого Імені', async () => {
+        await registrationPage.fillRegistrationForm('J', 'Doe', `${EMAIL_PREFIX}-${Date.now()}@${TEST_DOMAIN}`, 'Test1234', 'Test1234');
+        await expect(registrationPage.page.locator('text="Name has to be from 2 to 20 characters long"')).toBeVisible();
     });
 
-    
-    test('Перевірка на не співпадіння паролів', async ({ page }) => {
-        await page.fill('input[name="name"]', 'John');
-        await page.fill('input[name="lastName"]', 'Doe');
-        await page.fill('input[name="email"]', `${EMAIL_PREFIX}-${Date.now()}@${TEST_DOMAIN}`);
-        await page.fill('input[name="password"]', 'TestPas@@word22');
-        await page.fill('input[name="repeatPassword"]', 'TestPas@@word22Wrong');
-        const nameInput = page.locator('input[name="name"]');
-        await nameInput.focus();
-        await expect(page.locator('text="Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"')).toBeVisible();
+    test('Перевірка на не співпадіння паролів', async () => {
+        await registrationPage.fillRegistrationForm('John', 'Doe', `${EMAIL_PREFIX}-${Date.now()}@${TEST_DOMAIN}`, 'Test1234', 'WrongPassword');
+        await registrationPage.focusNameInput()
+        await expect(registrationPage.page.locator('text="Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"')).toBeVisible();
     });
-
-    
-
 });
